00293c569a6c7ee538758335757c7bd5b3eaad04
BOUFATAH AMINE <amine3100.boufatah@hotmail.com>
2019-10-27 18:38:50 +0100

    Clean code: Use constants

--- src/main/java/com/mbh/springsecexample/SecurityConfiguration.java
+++ src/main/java/com/mbh/springsecexample/SecurityConfiguration.java
@@ -53,8 +53,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()//
-        .antMatchers("/admin").hasRole("ADMIN")//
-        .antMatchers("/user").hasAnyRole("ADMIN", "USER")//
+        .antMatchers("/admin").hasRole(ROLE_ADMIN)//
+        .antMatchers("/user").hasAnyRole(ROLE_ADMIN, ROLE_USER)//
         .antMatchers("/").permitAll()//
         .and().formLogin();
   }
